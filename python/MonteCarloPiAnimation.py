de6e2dd1540bce53e933a6a65d7fe38f729cd877
54stickers <101305823+54stickers@users.noreply.github.com>
2022-03-22 23:47:35 -0400

    Update MonteCarloPiAnimation.py

--- python/MonteCarloPiAnimation.py
+++ python/MonteCarloPiAnimation.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-#-------------------------------------------------------------------------------#
-#	Author: Evan M. Cornuelle 													#	
-#	Date: 02/27/2022															#
+#-------------------------------------------------------------------------------
+#	Author: Evan M. Cornuelle 						
+#	Date: 02/27/2022							
 #																				#
-#	Description: Animnate a Monte-Carlo simulation for the calculation of Pi	#
+#	Description: Animnate a Monte-Carlo simulation for the calculation of Pi
 #																				#
-#-------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------
 
 #Global variables											
-N=1000															#Number of data points
-time = 10														#Animation time in s !not working!
-dt = time/N 													#Time incremebt for animation !not working!
-numerator = []													#Inside circle array
+N=1000											#Number of data points
+time = 10										#Animation time in s !not working!
+dt = time/N 										#Time incremebt for animation !not working!
+numerator = []										#Inside circle array
 
 #Set up the figure pane
 fig, ax = plt.subplots(1,figsize=(7, 7))						#Set figure size				
-ax.set_aspect('equal')											#Set the aspect ratio of the figure to 1:1
+ax.set_aspect('equal')									#Set the aspect ratio of the figure to 1:1
 ax.set_xlim(0, 1), ax.set_xticks([])							#Set axes limits to [0,1] and remove all tics
 ax.set_ylim(0, 1), ax.set_yticks([])
 
@@ -29,23 +29,23 @@ points = np.random.uniform(0,1,(N,2))							#Generate random (x,y) values
 
 
 #Construct the scatter plot for animation
-a = np.linspace(0,1,1000)										#Create x-data to plot circle
-b = (1 - a**2)**(1/2)											#Calculate y-data for circle
-ax.plot(a,b,'k-')												#Plot circle as a black line
+a = np.linspace(0,1,1000)								#Create x-data to plot circle
+b = (1 - a**2)**(1/2)									#Calculate y-data for circle
+ax.plot(a,b,'k-')									#Plot circle as a black line
 
-scatter, = ax.plot([],[],'o',markersize=3,color='#1c9099')		#Empty scatter plot for animation
-historyX, historyY = [], []										#Empty list to store animation history
+scatter, = ax.plot([],[],'o',markersize=3,color='#1c9099')				#Empty scatter plot for animation
+historyX, historyY = [], []								#Empty list to store animation history
 
 
 #Update plot with rolling calculation of Pi
 pi_template = r'$\pi = \frac{%i}{%i} = %f$'
 text = ax.text(0.1, 0.9,'',math_fontfamily='dejavuserif')
 
-for i in range(N):												#Find if random point is inside circle
+for i in range(N):									#Find if random point is inside circle
 	randomPointLength = points[i,0]**2 + points[i,1]**2
-	numerator.append(not(np.floor(randomPointLength)))			#If floor == 0, append 1. If floor == 1 append 0
+	numerator.append(not(np.floor(randomPointLength)))				#If floor == 0, append 1. If floor == 1 append 0
 
-num = np.cumsum(numerator)										#Number of points in circle is cumsum(numerator)
+num = np.cumsum(numerator)								#Number of points in circle is cumsum(numerator)
 
 
 
